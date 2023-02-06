9a7c140e414b8391edf054e41d9c5ee42a5c1d63
varna <grishpaapu24@gmail.com>
2023-02-06 12:58:48 +0530

    Grish resolved the conflicts

--- src/org/com/Desktop.java
+++ src/org/com/Desktop.java
@@ -15,16 +15,11 @@ public class Desktop implements Hardware , Software{
     private void desktop() {
 		System.out.println("Windows");
 		System.out.println("pushed by ajju");
+      System.out.println("pushed by grish");
 	
-<<<<<<< HEAD
-      private void desktop() {
-		
-    	  System.out.println("pushed by grish");
-	}
 
-=======
-}	
->>>>>>> 5e11703fc8e6c3841173857dacb518d40fa5667f
+    }
+
 
 public static void main(String[] args) {
 	
